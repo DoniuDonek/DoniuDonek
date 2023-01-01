5a2fc52e7226e73afc4fc865b9241a175ca8b789
DoniuDonek <awalach@wp.pl>
2023-01-01 15:41:03 +0100

    modified cache and checkout for authenticated users

--- accounts/views.py
+++ accounts/views.py
@@ -13,6 +13,11 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 from django.views.decorators.csrf import csrf_protect
 
+from cart.models import Cart, CartItem
+from cart.views import _cart_id
+
+import requests
+
 
 @csrf_protect
 def register(request):
@@ -50,18 +55,74 @@ def register(request):
     }
     return render(request, 'accounts/register.html', context)
 
+@csrf_protect
 def login(request):
     if request.method == 'POST':
-            email = request.POST['email']
-            password = request.POST['password']
-            user = auth.authenticate(email=email, password=password) # returns user object
-            if user is not None:
-                auth.login(request, user)
-                messages.success(request, 'You are now logged in')
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password) # returns user object    
+        
+        if user is not None:    
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists() 
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    #getting product variation by cart_id
+                    product_variation = []
+                    for item in cart_item:
+                        variation =  item.variations.all() #same way as cart/views.py
+                        product_variation = []
+                        for item in cart_item:
+                            variation = item.variations.all()
+                            product_variation.append(list(variation))
+    
+            # getting cart items from user to access his product variations
+                        cart_item = CartItem.objects.filter(user=user) # we dont need product=product as an argument - only checking if variation already exists
+                        ex_var_list = []                               # user = user as authenticated above
+                        id = []
+                        for item in cart_item:  
+                            existing_variation = item.variations.all()
+                            ex_var_list.append(list(existing_variation))
+                            id.append(item.id)
+
+                        # checking common product variation:
+                        for pr in product_variation:
+                            if pr in ex_var_list:
+                                index = ex_var_list.index(pr)
+                                item_id = id[index] 
+                                item=CartItem.objects.get(id=item_id)
+                                item.quantity += 1
+                                item.user = user
+                                item.save()
+                            else:
+                                cart_item=CartItem.objects.filter(cart=cart)
+                                for item in cart_item:
+                                    item.user = user
+                                    item.save()
+
+            except:
+                pass
+            
+            
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                # print('query --> ', query)
+                # query -->  next=/cart/checkout/   -->  make a split in dict --> next as a key, right side as value
+                params = dict(x.split('=') for x in query.split('&'))
+                # print('params --> ', params)    # params -->  {'next': '/cart/checkout/'}
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+                
+            except: 
                 return redirect('dashboard')
-            else:
-                messages.error(request, 'Invalid login credentials')
-                return redirect('login')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
 
     return render(request, 'accounts/login.html')
 
